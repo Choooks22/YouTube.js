d3b48be742f0a368c2d5337bb9bd880c8c3ab014
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-09-26 15:07:52 +0800

    📦 NEW: setup deno entrypoint

---
+++ compile.ts
@@ -0,0 +1,77 @@
+import { build, emptyDir } from 'https://deno.land/x/dnt@0.30.0/mod.ts';
+import { join } from 'https://deno.land/std@0.148.0/path/mod.ts';
+import { rollup } from 'https://esm.sh/rollup@2.79.1';
+
+const outDir = './npm';
+const copy = (source: string) => Deno.copyFile(source, join(outDir, source));
+
+await emptyDir(outDir);
+await Promise.all([
+  copy('./LICENSE'),
+  copy('./CONTRIBUTING.md'),
+  copy('./README.md'),
+]);
+
+await build({
+  entryPoints: ['./index.ts'],
+  outDir,
+  shims: {
+    deno: true,
+  },
+  package: {
+    name: 'youtubei.js',
+    version: '3.0.0',
+    license: 'MIT',
+    author: 'LuanRT <luan.lrt4@gmail.com> (https://github.com/LuanRT)',
+    homepage: 'https://github.com/LuanRT/YouTube.js#readme',
+    repository: {
+      'type': 'git',
+      'url': 'git+https://github.com/LuanRT/YouTube.js.git',
+    },
+    contributors: [
+      'Wykerd (https://github.com/wykerd/)',
+      'MasterOfBob777 (https://github.com/MasterOfBob777)',
+      'patrickkfkan (https://github.com/patrickkfkan)',
+    ],
+    bugs: {
+      'url': 'https://github.com/LuanRT/YouTube.js/issues',
+    },
+    funding: [
+      'https://github.com/sponsors/LuanRT',
+    ],
+    keywords: [
+      'yt',
+      'dl',
+      'ytdl',
+      'youtube',
+      'youtubedl',
+      'youtube-dl',
+      'youtube-downloader',
+      'youtube-music',
+      'innertubeapi',
+      'innertube',
+      'unofficial',
+      'downloader',
+      'livechat',
+      'studio',
+      'upload',
+      'ytmusic',
+      'search',
+      'comment',
+      'music',
+      'api',
+    ],
+  },
+});
+
+console.info('[rollup] Bundling for web...');
+const bun = await rollup({
+  input: './npm/esm/mod.js',
+});
+
+console.info('[rollup] Emitting web bundle...');
+await bun.write({
+  file: './npm/browser.js',
+});
+
+console.info('[rollup] Complete!');
